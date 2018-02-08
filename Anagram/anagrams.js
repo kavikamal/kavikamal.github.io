5ccbbf325d1774739073476c31306e7b3853900e
Kavitha Kamal <kavithakamal@gmail.com>
2018-02-08 13:35:56 -0500

    Updated Anagram file

--- Anagram/anagrams.js
+++ Anagram/anagrams.js
@@ -1,5 +1,23 @@
-document.getElementById("countButton").onclick = function() {
-   
-    var typedText = document.getElementById("textInput").value;
+function getAnagram() {
     
+    document.getElementById("anagram2").innerHTML="";
+    var typedText = document.getElementById("textInput").value;
+    var anagramWordList="";
+    var j=0;
+    for(word in words) {
+        if (alphabetize(typedText)==alphabetize(words[word])){
+            anagramWordList+= "  "+words[word]+", ";
+            j++;
+        }
+     }
+     anagramWordList=anagramWordList.slice(0,-2);
+     var span = document.createElement("span");
+        var textContent = document.createTextNode('List of Anagrams :' + anagramWordList );
+        span.appendChild(textContent);
+        document.getElementById("anagram2").appendChild(span);
  }
+ 
+ 
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+}
