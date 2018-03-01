c07724201ec28810ec696b8a9e91d66c9aa4188d
Kavitha Kamal <kavithakamal@gmail.com>
2018-03-01 16:45:24 -0500

    Added ListenKeyBoard

--- ListenKeyBoard/listenKeyBoard.js
+++ ListenKeyBoard/listenKeyBoard.js
@@ -4,25 +4,28 @@ var boxleft=200;
 
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  if (keyName=="ArrowDown"){
-  boxtop=boxtop+10;    
-  document.getElementById("box").style.top = boxtop + "px";
-  document.getElementById("box").style.backgroundImage= "url(smiley1.jpeg)";
-  }
-  else if (keyName=="ArrowUp"){
-    boxtop=boxtop-10;    
-    document.getElementById("box").style.top = boxtop + "px";
-    document.getElementById("box").style.backgroundImage= "url(smiley2.jpeg)";
+  
+  if ((keyName=="ArrowDown") && (window.innerHeight-200)>boxtop){
+      boxtop=boxtop+10;    
+      document.getElementById("box").style.top = boxtop + "px";
+      document.getElementById("box").style.backgroundImage= "url(smiley1.jpeg)";
     }
-  else if (keyName=="ArrowLeft"){
-        boxleft=boxleft-10;    
-        document.getElementById("box").style.left = boxleft + "px";
-        document.getElementById("box").style.backgroundImage= "url(smiley3.jpeg)";
-    } 
+  else if ((keyName=="ArrowUp")&& (boxtop>0)){
+      boxtop=boxtop-10;    
+      console.log(boxtop)
+      document.getElementById("box").style.top = boxtop + "px";
+      document.getElementById("box").style.backgroundImage= "url(smiley2.jpeg)";
+      }
+     
+  else if ((keyName=="ArrowLeft")&& (boxleft>0)){
+          boxleft=boxleft-10;    
+          document.getElementById("box").style.left = boxleft + "px";
+          document.getElementById("box").style.backgroundImage= "url(smiley3.jpeg)";
+      } 
   else if (keyName=="ArrowRight"){
-            boxleft=boxleft+10;    
-            document.getElementById("box").style.left = boxleft + "px";
-            document.getElementById("box").style.backgroundImage= "url(smiley4.jpeg)";
-    }      
+              boxleft=boxleft+10;    
+              document.getElementById("box").style.left = boxleft + "px";
+              document.getElementById("box").style.backgroundImage= "url(smiley4.jpeg)";
+      }      
   console.log('keydown event\n\n' + 'key: ' + keyName);
 });
